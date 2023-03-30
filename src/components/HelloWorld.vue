b4f3dcd327f0f9f5e305a54e4237d9d4bb75ddb8
Claydson Fernando <claydson.fernando@gmail.com>
2023-03-30 16:17:19 -0300

    feat: init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    Entra, faz o que tem que fazer e sai.
+  </div>
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Edu+NSW+ACT+Foundation:wght@600&display=swap');
+
+  body {
+    background: #000;
+  }
+
+  .hello {
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-family: 'Delicious Handrawn', cursive;
+    font-size: 45px
+  }
+</style>
